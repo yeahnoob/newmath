6460d67c6013ec366af6c170a94ad680dbd281c4
Xanadu Zhang <zgemlbox@gmail.com>
2014-07-10 17:04:32 +0800

    Fix the test. Use "go test github.com/yeahnoob/newmath" to run the test.

--- sqrt_test.go
+++ sqrt_test.go
@@ -1,11 +1,11 @@
-package main
+package newmath
 
 import (
     "testing"
 )
 
 func TestSqrt(t *testing.T) {
-    const in, out = 4, 2
+    const in, out = 81, 9
     if x := Sqrt(in); x != out {
         t.Errorf("Sqrt(%v) = %v,want %v", in, x, out)
     }
